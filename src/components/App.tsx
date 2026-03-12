776f5808cfa75bbcb2f42a7ab9101b740a25806e
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-03-12 20:53:36 +0600

    feature: initiate monoco-editor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,26 @@
+import Editor, { type Monaco } from "@monaco-editor/react";
+import { useRef } from "react"
+
 const App = () => {
+  const editorRef = useRef<Monaco>(null);
+
+  const handleEditorDidMount = (editor:any, monoco : any) => {
+    editorRef.current = editor;
+  }
+  const showValue = () => {
+    alert(editorRef.current!.getValue());
+  }
+
   return (
     <div>
-      
+      <button onClick={showValue}>Show value</button>
+      <Editor
+        height="50vh"
+        width={"60vw"}
+        defaultLanguage="javascript"
+        defaultValue="//Hi , Welcome to revoirt"
+        theme="dark"
+        onMount={handleEditorDidMount} />
     </div>
   )
 }
--- src/router/Route.tsx
+++ src/router/Route.tsx
@@ -3,12 +3,11 @@ import Home from "../components/Home";
 import App from "../components/App";
 
 const router = createBrowserRouter([{
-    path : "/" ,
-    element : <Home />,
-    children : [{
-        path : "/app",
-        element : <App />
-    }]
+    path: "/",
+    element: <Home />,
+}, {
+    path: "/app",
+    element: <App />
 }])
 
-export default router ;
+export default router;
