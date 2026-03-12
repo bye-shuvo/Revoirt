4bb21aca90ca782a82c1e9bc8aa4067c05133b15
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-03-13 01:57:11 +0600

    feature : initialize navigation and terminal

--- src/components/FileExplorar.tsx
+++ src/components/FileExplorar.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import { useState } from "react"
 
-const FileExplorar = () => {
+interface file {
+  name: string,
+  content: string
+}
+const files: file[] = [{
+  name: "index.js",
+  content: ""
+}, {
+  name: "utils.js",
+  content: ""
+}]
+const FileExplorar = ({ value }: { value: string }) => {
+  const [selectedFile, setSelectedFile] = useState(0);
+  const handleFileChange = (e: React.MouseEvent<HTMLLIElement>) => {
+    setSelectedFile(Number(e.currentTarget.dataset.index));
+    files[Number(e.currentTarget.dataset.index)].content = value;
+  }
+
   return (
-    <div className='h-[95%] w-[20%]'>
-      
-    </div>
+    <aside className='h-full w-[15%] bg-amber-300'>
+      <ul className="flex flex-col justify-center w-full">
+        {
+          files.map((file, index) => {
+            return <li key={index} data-index={index} aria-braillelabel={file.name} onClick={handleFileChange} className={`p-2 text-md hover:bg-gray-700/50 ${selectedFile === index ? "bg-gray-700" : "bg-none"}`}>{file.name}</li>
+          })
+        }
+      </ul>
+    </aside>
   )
 }
 
