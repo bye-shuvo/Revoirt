4341ce68ec152185f2cdd1cec08bd42771012ee2
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-03-14 22:29:38 +0600

    feature : Added resizable panels for revoirt editor

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,6 +1,6 @@
 const Terminal = () => {
   return (
-    <footer className="absolute top-[90%] left-[5%] bg-green-400 w-[80%]">
+    <footer className="bg-green-400 w-full h-full">
       
     </footer>
   )
