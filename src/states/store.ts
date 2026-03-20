50c47a1b9c7a5d0269d0fc808517287e34891c70
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-03-20 23:32:56 +0600

    Update: Implemented file delete logic with proper context menu

--- src/states/store.ts
+++ src/states/store.ts
@@ -1,16 +1,5 @@
 import { create } from "zustand";
 
-type ValueState = {
-  value: string;
-  setValue: (nextValue: string) => void;
-};
-
-export const useValue = create<ValueState>()((set) => ({
-    value : "" ,
-    setValue : (nextValue) => set({ value: nextValue })
-}));
-
-
 type path = {
     path : string ,
     setPath : (next : string) => void ;
