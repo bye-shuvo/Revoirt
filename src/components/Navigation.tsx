2d3b1001a57eb83364cb8519be2c9e5a49caeef4
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-04-22 02:54:00 +0600

    Error : Implemented yjs-monaco for collaborative editor with error

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 const Navigation = () => {
   return (
-    <nav className="bg-slate-900 h-[6%] w-full flex justify-between items-center px-3">
+    <nav className="bg-mist-900 h-[6%] w-full flex justify-between items-center px-3">
       <h1 className='font-saira-stencil-one text-2xl text-white' title="Code together , Build Fast">REVOIRT</h1>
       <div></div>
       <div className="rounded-2xl py-1 px-2 bg-green-500/80 text-sm inset-1 border-b-2 border-green-500/90">Custom navigation toasts will appear here</div>
