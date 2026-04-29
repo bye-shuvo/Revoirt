98db358bdbe75c0b40ed3f6fbc1649ea76a9ef2f
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-04-29 21:54:33 +0600

    Update : Added logic for collaboration modal trigger

--- src/components/utility/Navigation.tsx
+++ src/components/utility/Navigation.tsx
@@ -1,10 +1,25 @@
+import { useIsCollaborating } from "../../states/store"
+
 const Navigation = () => {
+
+  const setIsCollaborating = useIsCollaborating((state) => state.setIsCollaborating);
+
   return (
-    <nav className="bg-mist-900 h-[7%] w-full flex justify-between items-center px-3">
-      <h1 className='font-saira-stencil-one text-2xl text-white' title="Code together , Build Fast">REVOIRT</h1>
-      <div></div>
-      <div className="rounded-2xl py-1 px-2 bg-green-500/80 text-sm inset-1 border-b-2 border-green-500/90">Custom navigation toasts will appear here</div>
-      <div className="object-center object-cover h-8 w-8 bg-green-300 rounded-full"></div>
+    <nav className="bg-mist-900 h-[7%] w-full flex items-center">
+      <ul className="w-full flex justify-between items-center px-3">
+        <li id="logo"><h1 className='font-saira-stencil-one text-3xl text-white cursor-pointer' title="Code together | Build Fast">REVOIRT</h1></li>
+        <li id="right-utils" className="flex items-center gap-10">
+          <div id="joined-users" className="flex relative justify-center items-center w-fit h-full">
+            {
+              Array.from({ length: 5 }).map((_, index) => {
+                return <span key={index} className={`relative h-8 w-8 flex rounded-full bg-purple-500 border border-green-400`} style={{ left: index * -10 }}> <img src="" alt={`user-${index+1}`} /> </span>
+              })
+            }
+          </div>
+          <button id="collaborate" className="bg-sky-500 border-b-3 border-sky-700 cursor-pointer p-2 active:border-0" onClick={() => setIsCollaborating(true)}>Collaborate</button>
+          <button id="profile" title="navigate profile" className="object-center object-cover h-9 w-9 bg-green-300 rounded-full cursor-pointer"></button>
+        </li>
+      </ul>
     </nav>
   )
 }
