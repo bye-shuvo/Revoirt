8b2a52711ffc3d335648d26ea665d79029e11731
Bye-Shuvo <abuhasnatshuvo605@gmail.com>
2026-05-01 13:11:56 +0600

    Feature : Added room Id hashing feature for editor room generation

---
+++ src/utills/services/hashRoomId.ts
@@ -0,0 +1,15 @@
+import crypto from 'crypto-js'
+const serect_key = import.meta.env.VITE_FILES_SECRECT_KEY ;
+
+export const encryptRoomId = async (roomId : string) => {
+    const hash = crypto.AES.encrypt(roomId, serect_key).toString();
+    await window.cookieStore.set('files' , hash);
+    return hash ;
+}
+
+export const decryptRoomId = () => {
+    const roomIdHashFragment = window.location.hash ;
+    const roomIdHash = roomIdHashFragment.split("=").at(-1)?.split("-").at(-1);
+    if(!roomIdHash) return ;
+    return roomIdHash ;
+}
